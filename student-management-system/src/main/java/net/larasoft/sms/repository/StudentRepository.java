db483f2f3c5e9367b9763e8fc81ab2b26ff97ae2
laurajun <31452148+laurajun@users.noreply.github.com>
2021-07-15 17:48:13 +0300

    Base for Spring Boot Student Management System

--- student-management-system/src/main/java/net/larasoft/sms/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package net.larasoft.sms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.larasoft.sms.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-
-}
